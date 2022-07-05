faa0b5290499502ac6802c973c53aec2898b165a
Qadri <93231723+rewadevs@users.noreply.github.com>
2022-07-05 22:21:50 +0800

    Add files via upload

---
+++ compfestq/website/auth.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        idsiswa = request.form.get('idsiswa')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(idsiswa=idsiswa).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Berhasil Masuk!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password salah, coba lagi.', category='error')
+        else:
+            flash('ID Siswa belum terdaftar, klik tombal daftar', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        idsiswa = request.form.get('idsiswa')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(idsiswa=idsiswa).first()
+        if user:
+            flash('ID sudah terdaftar, masuk melalui login.', category='error')
+        elif len(idsiswa) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(idsiswa=idsiswa, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('ID Terdaftar!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
+
